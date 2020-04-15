0e01d556d24fab90d33d140e365eb5f4ea6d3fbd
fishysticks456 <fishysticks456@gmail.com>
2020-04-15 09:15:34 -0700

    Pre-refactoring
    Refactored Adobe and utag_data settings into something more modular, turned off utag_data and digitalData, set up Pinterest tracking. Going to make more changes and add a config.js file.

--- roam.js
+++ roam.js
@@ -9,14 +9,88 @@ const Console = require('console'); // Outputting to console
 const readline = require('readline'); // Reading keyboard inputs on console
 const chalk = require('chalk'); // Command prompt styling and colors
 const inquirer = require('inquirer'); // Command line interface that asks questions and parses input 
+const brands = require('./qa_env_accounts.js'); // Usernames and passwords of each brand QA env
+
+const devices = require('puppeteer/DeviceDescriptors');
+const iPhone = devices['iPhone X'];
 
 // EDIT CONFIGURATION HERE --------------
 const browserWidth = 1100;
 const browserHeight = 800;
-const showUtag = true;  	// Set to false to suppress utag_data on console.
-const showAdobe = true; 	// Set to false to suppress Adobe on console.
-const showDigitalData = true; 	// Set to false to suppress digitalData on console.
 const generatePrompt = true; // Set to false to turn off the starting prompt
+
+// Data points are network requests or browser variables that you want to capture on each page/request
+// If it's a network request, it's search parameters will be separated into columns
+// If it's a browser variable object, it's properties will be separated into columns
+const dataPoints = {
+	"Adobe" : {
+		"networkKey" : (url) => url.indexOf("b/ss/") > -1,
+		"extraColumns" : function(req, row) {
+			if( row["events"] ) {
+				row["nonTimingEvents"] = row["events"].split("event231")[0];
+			}
+		},
+		"showInConsole" : true,
+		"recordInExcel" : true,
+		"impVars" : [
+			"pageName",
+			"nonTimingEvents", 
+			"purchaseID",
+			"products",
+			"v33",
+			"v50",
+			"v48",
+			"v4",
+			"g"
+		]
+	},
+	"Pinterest" : {
+		"networkKey" : (url) => url.indexOf("pinterest") > -1 && url.indexOf("event=") > -1,
+		"extraColumns" : function(req, row) {
+			//console.log(req.headers())
+			if( row["ed"] ) {
+				//console.log("collecting ed for Pinterest");
+				var ed = JSON.parse(row["ed"]);
+				for( var d in ed ) {
+					row[d] = ed[d];
+				}
+			}
+			var header = req.headers();
+			row["ref.url"] = header && header.referer ? header.referer : "";
+		},
+		"showInConsole" : true,
+		"recordInExcel" : true,
+		"impVars" : [
+			"ref.url",
+			"event",
+			"page_title"
+		]
+	}
+	/*,
+	"utag_data" : {
+		"browserVariable" : "utag_data",
+		"showInConsole" : false,
+		"recordInExcel" : false,
+		"impVars" : [
+			"page_name",
+			"page_type",
+			"pmc_pageName",
+			"previous_page_name"
+		]
+	},
+	"digitalData" : {
+		"browserVariable" : "digitalData",
+		"showInConsole" : false,
+		"recordInExcel" : false,
+		"impVars" : [
+			"page"
+		]
+	}*/
+}
+/*
+const showUtag = false;  	// Set to false to suppress utag_data on console.
+const showAdobe = true; 	// Set to false to suppress Adobe on console.
+const showDigitalData = false; 	// Set to false to suppress digitalData on console.
 const utagImpVars = [
 	"page_name",
 	"geo",
@@ -25,35 +99,18 @@ const utagImpVars = [
 ];
 const adobeImpVars = [
 	"pageName",
-	"server",
-	"ch",
 	"events", 
 	"purchaseID",
-	"products"
+	"products",
+	"v50",
+	"v4",
+	"g"
 ];
 const digitalDataImpVars = [
 	"page"
-]
-// const brands = [
-// 	"williams-sonoma",
-// 	"potterybarn",
-// 	"potterybarnkids",
-// 	"pbteens",
-// 	"westelm",
-// 	"markandgraham",
-// 	"rejuvenation"
-// ];
-/*const brands = {
-	"WS" : "williams-sonoma",
-	"PB" : "potterybarn",
-	"PK" : "potterybarnkids",
-	"PT" : "pbteens",
-	"WE" : "westelm",
-	"MG" : "markandgraham",
-	"RJ" : "rejuvenation"
-};*/
+]*/
 
-const brands = require('./qa_env_accounts.js');
+
 const brand_env = Object.keys(brands);
 const brandCode = "WS"; // default brand
 var all_env = {
@@ -98,19 +155,20 @@ var excel_filename = "roam"; // Excel filename format: "[excel_filename]_[env]_[
 // END CONFIGURATION ------------------
 
 let roam = async () => {
-	var utagResults = [];
-	var adobeResults = [];
-	var digitalDataResults = [];
+	// var utagResults = [];
+	// var adobeResults = [];
+	// var digitalDataResults = [];
 
 	async function grabAnalyticsData() {
 		// Listening to Adobe requests by setting RequestInterception to true
 		await page.setRequestInterception(true);
 		page.on('request', async req => {
 		   	// Listen for any Adobe request containing "b/ss"
+		   /*
 		    if (req.url().indexOf("b/ss") > -1 ) {
 		    	// Check if they have POST data
 		    	var post = req.postData() || req.url();
-		    	var post = post.indexOf("?") > -1 ? post.split("?")[1] : post;
+		    	post = post.indexOf("?") > -1 ? post.split("?")[1] : post;
 		    	var params = post.split("&");
 		    	var param, impAValues = {}, row = {};
 
@@ -131,10 +189,51 @@ let roam = async () => {
 		        adobeResults.push(row);  
 
 		        // Grab utag_data
-		        await grabUtagData();
+		        if( showUtag || showDigitalData ) await grabUtagData();
+		    } */
 
-		    // Listen for Mixpanel requests containing "?data"
-		    } 
+		    for(var d in dataPoints) {
+		    	if( typeof dataPoints[d].networkKey != 'undefined' && dataPoints[d].networkKey(req.url()) ) {
+		    		var post = req.postData() || req.url();
+			    	post = post.indexOf("?") > -1 ? post.split("?")[1] : post;
+			    	var params = post.split("&");
+			    	var param, impAValues = {}, row = {};
+
+			    	// Capture the request url without search parameters
+			    	row["req.url"] = req.url().split("?")[0];
+
+			        for( var i = 0; i < params.length; i++) {
+			        	param = params[i].split("=");
+			        	row[param[0]] = decodeURIComponent(param[1]);
+			        	
+			        }
+			        if( dataPoints[d].extraColumns ) dataPoints[d].extraColumns(req, row);
+			        
+			        for( var j = 0; j < dataPoints[d].impVars.length; j++ ) {
+			        	var a = dataPoints[d].impVars[j];
+			        	if( typeof row[a] != 'undefined' ) {
+			        		impAValues[a] = row[a];
+			        	}
+			        }
+			        //var reg = RegExp(/\/b\/ss\/([^\/]*)\//g);
+			        //row["Report Suite ID"] = reg.exec(req.url(), "$1")[1];
+			        if( dataPoints[d].showInConsole ) {
+				        Console.log(chalk.bgRed(d));
+				        Console.log(impAValues);
+				    }
+			        //row["Full Request"] = req.url();
+			        dataPoints[d].results = dataPoints[d].results || [];
+			        dataPoints[d].results.push(row);
+
+			        // If HAR downloading is on, download file then turn it off.
+					if( har.on ) {
+						await page.waitFor( 3000 );
+						await har.stop();
+						har.on = false;
+						Console.log(chalk.yellow(">>> HAR file downloaded."));
+					}
+		    	}
+		    }
 
 		    req.continue();
 		});
@@ -148,67 +247,66 @@ let roam = async () => {
 			Console.error(err);
 			return;
 		});
-		await page.waitForFunction('typeof digitalData != "undefined"');
-		await page.waitForFunction('typeof utag != "undefined"');
-		// Grab utag_data from page
-		await page.evaluate( () => {
-			return utag_data;
-		}).then((data) => {
-			// Only print out the important utag_data variables
-			var impVars = {};
-			for(var d in data) {
-				if(  utagImpVars.includes(d) ) {
-					impVars[d] = data[d];
-				}
-				// If data's data is an object, stringify it to show on excel.
-				if( typeof data[d] === 'object') data[d] = JSON.stringify(data[d]);
-			}
-			// Add on full 
-			//row["Full utag_data"] = JSON.stringify(data);
-			utagResults.push( data );
-			if( showUtag ) {
-				Console.log(chalk.bgCyan("utag_data"));
-				Console.log(impVars);
-			}
-			//Console.log("Grabbed utag data" + ( typeof d["pagename"] != 'undefined' ? " at [" + d["page_name"] + "]" : ""));
-		}).catch( err => {
-        	Console.log("Couldn't grab utag_data on " + currentPage);
-        	Console.error(err);	
-        });;
+		if( showUtag ) {
 
-        // Grab digitalData from page
-		await page.evaluate( () => {
-			return digitalData;
-		}).then((data) => {
-			// Only print out the important utag_data variables
-			var impVars = {};
-			for(var d in data) {
-				if(  digitalDataImpVars.includes(d) ) {
-					impVars[d] = data[d];
-				}
-				// If data's data is an object, stringify it to show on excel.
-				if( typeof data[d] === 'object') data[d] = JSON.stringify(data[d]);
-			}
-			// Add on full 
-			//row["Full utag_data"] = JSON.stringify(data);
-			digitalDataResults.push( data );
-			if( showDigitalData ) {
-				Console.log(chalk.bgCyan("digitalData"));
-				Console.log(impVars);
-			}
-			
-		}).catch( err => {
-        	Console.log("Couldn't grab digitalData on " + currentPage);
-        	Console.error(err);	
-        });;
 
-        // If HAR downloading is on, download file then turn it off.
-		if( har.on ) {
-			await page.waitFor( 3000 );
-			await har.stop();
-			har.on = false;
-			Console.log(chalk.yellow(">>> HAR file downloaded."));
+			await page.waitForFunction('typeof digitalData != "undefined"');
+			await page.waitForFunction('typeof utag != "undefined"');
+			// Grab utag_data from page
+			await page.evaluate( () => {
+				return utag_data;
+			}).then((data) => {
+				// Only print out the important utag_data variables
+				var impVars = {};
+				for(var d in data) {
+					if(  utagImpVars.includes(d) ) {
+						impVars[d] = data[d];
+					}
+					// If data's data is an object, stringify it to show on excel.
+					if( typeof data[d] === 'object') data[d] = JSON.stringify(data[d]);
+				}
+				// Add on full 
+				//row["Full utag_data"] = JSON.stringify(data);
+				utagResults.push( data );
+				if( showUtag ) {
+					Console.log(chalk.bgCyan("utag_data"));
+					Console.log(impVars);
+				}
+				//Console.log("Grabbed utag data" + ( typeof d["pagename"] != 'undefined' ? " at [" + d["page_name"] + "]" : ""));
+			}).catch( err => {
+	        	Console.log("Couldn't grab utag_data on " + currentPage);
+	        	Console.error(err);	
+	        });
 		}
+
+		if( showDigitalData ) {
+	        // Grab digitalData from page
+			await page.evaluate( () => {
+				return digitalData;
+			}).then((data) => {
+				// Only print out the important utag_data variables
+				var impVars = {};
+				for(var d in data) {
+					if(  digitalDataImpVars.includes(d) ) {
+						impVars[d] = data[d];
+					}
+					// If data's data is an object, stringify it to show on excel.
+					if( typeof data[d] === 'object') data[d] = JSON.stringify(data[d]);
+				}
+				// Add on full 
+				//row["Full utag_data"] = JSON.stringify(data);
+				digitalDataResults.push( data );
+				if( showDigitalData ) {
+					Console.log(chalk.bgCyan("digitalData"));
+					Console.log(impVars);
+				}
+				
+			}).catch( err => {
+	        	Console.log("Couldn't grab digitalData on " + currentPage);
+	        	Console.error(err);	
+	        });
+		}
+
 	}
 
 	// Setting up keyboard press bindings for console input
@@ -244,31 +342,39 @@ let roam = async () => {
 	}
 
 	function writeToSpreadsheet() {
-		if( !utagResults || !adobeResults || !digitalDataResults ) {
-			Console.error(chalk.red("Missing utag or adobe data to write to spreadsheet"));
-			return;
-		} else {
-			
-			//var data = []; // [[1,2,3],[true, false, null, "sheetjs"],["foo","bar",new Date("2014-02-19T14:30Z"), "0.3"], ["baz", null, "qux"]]
-			const finalEnv = brands[all_env.brand_env].id + "_" + qa_environments[all_env.qa_env];// + "_" + all_env.tealium_env;
-			const timestamp = getTimestamp();
-			var filename = excel_filename + "_" + timestamp + "_" + finalEnv;
+		
+		//var data = []; // [[1,2,3],[true, false, null, "sheetjs"],["foo","bar",new Date("2014-02-19T14:30Z"), "0.3"], ["baz", null, "qux"]]
+		const finalEnv = brands[all_env.brand_env].id + "_" + qa_environments[all_env.qa_env];// + "_" + all_env.tealium_env;
+		const timestamp = getTimestamp();
+		const filename = excel_filename + "_" + timestamp + "_" + finalEnv;
+		var ws;
 
-			const wb = XLSX.utils.book_new(), 
-			ws1 = XLSX.utils.json_to_sheet(utagResults, {header: utagImpVars});
-			ws2 = XLSX.utils.json_to_sheet(adobeResults, {header: adobeImpVars});
-			ws3 = XLSX.utils.json_to_sheet(digitalDataResults, {header: digitalDataImpVars});
-			 
-			/* add worksheet to workbook */
-			XLSX.utils.book_append_sheet(wb, ws1, "utag_data");
-			XLSX.utils.book_append_sheet(wb, ws3, "digitalData");
-			XLSX.utils.book_append_sheet(wb, ws2, "Adobe");
+		const wb = XLSX.utils.book_new();
 
-			/* write workbook */
-			XLSX.writeFile(wb, filename + ".xlsx");
-			doneWriting = true;
-			Console.log(chalk.yellow("Wrote data to " + filename + ".xlsx"));
+		/*
+		ws1 = XLSX.utils.json_to_sheet(utagResults, {header: utagImpVars});
+		ws2 = XLSX.utils.json_to_sheet(adobeResults, {header: adobeImpVars});
+		ws3 = XLSX.utils.json_to_sheet(digitalDataResults, {header: digitalDataImpVars});
+		*/ 
+
+		/* add worksheet to workbook */
+		for( d in dataPoints ) {
+			//console.log( dataPoints[d].results);
+			if( dataPoints[d].recordInExcel && typeof dataPoints[d].results != 'undefined' ) {
+				ws = XLSX.utils.json_to_sheet(dataPoints[d].results, {header: dataPoints[d].impVars})
+				XLSX.utils.book_append_sheet(wb, ws, d);
+			}
 		}
+
+		// if( showUtag ) XLSX.utils.book_append_sheet(wb, XLSX.utils.json_to_sheet(utagResults, {header: utagImpVars}), "utag_data");
+		// if( showDigitalData ) XLSX.utils.book_append_sheet(wb, XLSX.utils.json_to_sheet(digitalDataResults, {header: digitalDataImpVars}), "digitalData");
+		// if( showAdobe ) XLSX.utils.book_append_sheet(wb, XLSX.utils.json_to_sheet(adobeResults, {header: adobeImpVars}), "Adobe");
+
+		/* write workbook */
+		XLSX.writeFile(wb, filename + ".xlsx");
+		doneWriting = true;
+		Console.log(chalk.yellow("Wrote data to " + filename + ".xlsx"));
+	
 	}
 
 	async function promptShortcuts() {
@@ -278,7 +384,7 @@ let roam = async () => {
 		await inquirer.prompt({
 		    type: 'input',
 		    name: "key",
-		    message: "Type 'q' and press Enter to close Node process. Type 's' and press Enter to save a screenshot. Type 'h' to download the HAR file.\n"
+		    message: "Type 'q' and press Enter to close Node process. Type 's' and press Enter to save a screenshot. Type 't' to open the Tealium Web Companion. Type 'h' to download the HAR file.\n"
 	    }).then(async answer => {
 	    	if( answer.key && answer.key.toLowerCase() == 's' ) {
 	    		//var t = new Date().getTime();
@@ -293,6 +399,13 @@ let roam = async () => {
 				await har.start({ path: all_env.qa_env + "." + all_env.brand_env+ ".com." + getTimestamp() + ".har" });
 				//await har.stop();
 				Console.log(chalk.yellow(">>> HAR file will download the next page."));
+				promptShortcuts();
+				return;
+
+	    	} else if (answer.key && answer.key.toLowerCase() == 't' ) {
+	    		// Request the Tealium Web Companion plugin script.
+	    		await page.addScriptTag({url:"https://tags.tiqcdn.com/utui/utui.tagcompanion.js?v=" + Math.random()});
+				Console.log(chalk.yellow(">>> Requested Tealium Web Companion plugin."));
 				promptShortcuts();
 				return;
 
@@ -329,7 +442,7 @@ let roam = async () => {
 	// START //
 	///////////
 
-	Console.log(chalk.yellow("Starting up QA Tool - scrape.js"));
+	Console.log(chalk.yellow("Starting up QA Tool - roam.js"));
 	
 	promptShortcuts();
 
@@ -356,7 +469,8 @@ let roam = async () => {
 	})
 
 	// Start up the browser
-	await page.setViewport({width: browserWidth, height: browserHeight});
+	//await page.setViewport({width: browserWidth, height: browserHeight});
+	await page.emulate(iPhone);
 
 	// Turn on Adobe grabbing.
 	await grabAnalyticsData();
